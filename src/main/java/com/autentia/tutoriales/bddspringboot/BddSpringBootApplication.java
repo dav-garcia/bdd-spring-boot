4694aae1c2d8d467825082a8cf14e03483d42854
David García Gil <dgarciagil@autentia.com>
2018-09-30 08:57:50 +0200

    Add Spring Initializr project template.

---
+++ src/main/java/com/autentia/tutoriales/bddspringboot/BddSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.autentia.tutoriales.bddspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddSpringBootApplication.class, args);
+	}
+}
