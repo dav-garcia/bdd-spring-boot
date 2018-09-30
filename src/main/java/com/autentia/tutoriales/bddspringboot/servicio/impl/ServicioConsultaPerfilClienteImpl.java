811a3dbc533539f42edd4b61363fcec7aaf15c8b
David García Gil <dgarciagil@autentia.com>
2018-09-30 19:42:36 +0200

    Return constant customer profile.

--- src/main/java/com/autentia/tutoriales/bddspringboot/servicio/impl/ServicioConsultaPerfilClienteImpl.java
+++ src/main/java/com/autentia/tutoriales/bddspringboot/servicio/impl/ServicioConsultaPerfilClienteImpl.java
@@ -3,12 +3,19 @@ package com.autentia.tutoriales.bddspringboot.servicio.impl;
 import com.autentia.tutoriales.bddspringboot.servicio.ServicioConsultaPerfilCliente;
 import com.autentia.tutoriales.bddspringboot.vo.RespuestaPerfilCliente;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class ServicioConsultaPerfilClienteImpl implements ServicioConsultaPerfilCliente {
 
     @Override
     public RespuestaPerfilCliente consultar(UUID idCliente) {
-        return new RespuestaPerfilCliente();
+        return new RespuestaPerfilCliente.Builder()
+        .withId(idCliente)
+        .withNombre("David")
+        .withFechaNacimiento(LocalDate.of(1976,2,28))
+        .withEmail("dgarciagil@autentia.com")
+        .withTelefono("+34 123456789")
+        .build();
     }
 }
--- src/test/java/com/autentia/tutoriales/bddspringboot/servicio/ServicioConsultaPerfilClienteTest.java
+++ src/test/java/com/autentia/tutoriales/bddspringboot/servicio/ServicioConsultaPerfilClienteTest.java
@@ -4,6 +4,9 @@ import com.autentia.tutoriales.bddspringboot.servicio.impl.ServicioConsultaPerfi
 import com.autentia.tutoriales.bddspringboot.vo.RespuestaPerfilCliente;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -14,12 +17,17 @@ public class ServicioConsultaPerfilClienteTest {
     @Test
     public void dadoIdClienteExistenteEntoncesDevuelvePerfilCliente() {
         // Given
-        // ???
+        UUID id = UUID.randomUUID();
 
         // When
-        RespuestaPerfilCliente result = sut.consultar(null);
+        RespuestaPerfilCliente result = sut.consultar(id);
 
         // Then
         assertThat(result, is(not(nullValue())));
+        assertThat(result.getId(), is(id));
+        assertThat(result.getNombre(), is("David"));
+        assertThat(result.getFechaNacimiento(), is(LocalDate.of(1976, 2, 28)));
+        assertThat(result.getEmail(), is("dgarciagil@autentia.com"));
+        assertThat(result.getTelefono(), is("+34 123456789"));
     }
 }
